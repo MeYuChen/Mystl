91fba783ef41b5099063add17d5b19b7b4f6cb8e
chenyu <chenyu@unity-drive.com>
2023-12-06 17:10:30 +0800

    feat(mystl):移动文件，添加cmakelist

---
+++ include/iterator.h
@@ -0,0 +1,6 @@
+// Copyright (c) 2023, Yu Chen. All rights reserved.
+// Author: i_yuchen@outlook.com
+#pragma once
+#include <cstddef>
+
+namespace mystl {}  // namespace mystl
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2023, Yu Chen. All rights reserved.
+// Author: i_yuchen@outlook.com
+#include <iostream>
+
+#include "kk.h"
+#include "type_traits.h"
+int main(int argc, char** argv) {
+  std::cout << "hello world" << std::endl;
+  mystl::kk l;
+  return 0;
+}
